24914f92a476073b917aefd836bfcf3d284767fd
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-04-04 14:29:08 -0700

    springboot starter

---
+++ src/main/java/com/pivotguard/pivotguard_api/PivotguardApiApplication.java
@@ -0,0 +1,13 @@
+package com.pivotguard.pivotguard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotguardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivotguardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pivotguard/pivotguard_api/PivotguardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pivotguard.pivotguard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotguardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
