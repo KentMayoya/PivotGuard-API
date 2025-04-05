56c292cf3f22ef357033fba22a3c633f894343a2
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-04-04 17:37:30 -0700

    Create DTOs (safe site & threats)

---
+++ src/main/java/com/pivotguard/pivotguard_api/dtos/SafeSiteDto.java
@@ -0,0 +1,14 @@
+/**
+ * safe site Data transfer object to only display URL and 
+ * 
+ * @author Captain Chungus AKA Alan Talavera-Cordova
+ */
+package com.pivotguard.pivotguard_api.dtos;
+
+public interface SafeSiteDto {
+  String getURL();
+
+  String getName();
+
+  String getDescription();
+}
