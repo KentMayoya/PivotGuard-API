2ef52ac4210789a13b1402002f464574048c5140
Gabe Lapingcao <mgultra2018@gmail.com>
2025-04-05 09:18:26 -0700

    Broader query to get more URLs

--- src/main/java/com/pivotguard/pivotguard_api/repositories/SiteRepository.java
+++ src/main/java/com/pivotguard/pivotguard_api/repositories/SiteRepository.java
@@ -10,31 +10,31 @@ import java.util.List;
 
 public interface SiteRepository extends JpaRepository<Site, Integer> {
 
-  @Query (nativeQuery = true, value = """
+  @Query(nativeQuery = true, value = """
       SELECT EXISTS (
-        SELECT 1 FROM Site s WHERE s.URL = :currentSite AND s.iscompromised = TRUE
+        SELECT 1 FROM Site s WHERE (s.URL LIKE CONCAT('%' ,:currentSite, '%') OR :currentSite LIKE CONCAT('%', s.URL, '%')) AND s.iscompromised = TRUE
       );
       """)
-    Boolean isCompromised(String currentSite);
+  Boolean isCompromised(String currentSite);
 
-    @Query (nativeQuery = true, value = """
-    SELECT 
-      s.url as URL,
-      s.name as name,
-      s.description as description
-    FROM 
-      Site s 
-    WHERE 
-      ServiceType = (
-        SELECT 
-          ServiceType
-        FROM 
-          site s
-        WHERE 
-          s.url = :url
-        LIMIT 1
-      )
-      AND iscompromised = FALSE;
-    """)
-    List<SafeSiteDto> getSafeSites(String url);
+  @Query(nativeQuery = true, value = """
+      SELECT
+        s.url as URL,
+        s.name as name,
+        s.description as description
+      FROM
+        Site s
+      WHERE
+        ServiceType = (
+          SELECT
+            ServiceType
+          FROM
+            site s
+          WHERE
+            s.url = :url
+          LIMIT 1
+        )
+        AND iscompromised = FALSE;
+      """)
+  List<SafeSiteDto> getSafeSites(String url);
 }
