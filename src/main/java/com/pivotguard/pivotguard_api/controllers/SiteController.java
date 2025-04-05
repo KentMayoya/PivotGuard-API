482682e155036ecebc0e68dacb2be7da4419e5c1
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-04-04 22:16:58 -0700

    Added comments

--- src/main/java/com/pivotguard/pivotguard_api/controllers/SiteController.java
+++ src/main/java/com/pivotguard/pivotguard_api/controllers/SiteController.java
@@ -1,6 +1,10 @@
+/**
+ * Defines the API endpoints for querying the database.
+ * 
+ * @author Kent Mayoya
+ */
 package com.pivotguard.pivotguard_api.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.pivotguard.pivotguard_api.entities.ThreatType;
 import com.pivotguard.pivotguard_api.repositories.SiteRepository;
 import com.pivotguard.pivotguard_api.repositories.ThreatTypeRepository;
 import com.pivotguard.pivotguard_api.responses.CompromisedSiteResponse;
@@ -33,6 +36,7 @@ public class SiteController {
         return ResponseEntity.ok(result);
     }
 
+    // test on http://localhost:8080/api/v1/sites/getThreatTypes?url=www.google.com
     @GetMapping("/getThreatTypes")
     ResponseEntity<ThreatTypeResponse> getThreatTypes(@RequestParam String url) {
         ThreatTypeResponse response = new ThreatTypeResponse(threatTypeRepository.getThreatTypes(url));
--- src/main/java/com/pivotguard/pivotguard_api/entities/Site.java
+++ src/main/java/com/pivotguard/pivotguard_api/entities/Site.java
@@ -1,4 +1,8 @@
-
+/**
+ * Represents a Site.
+ * 
+ * @author Kent Mayoya
+ */
 package com.pivotguard.pivotguard_api.entities;
 
 import jakarta.persistence.Entity;
