4bfa1b00718d60763f47f1de49607f20a65315f1
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-04-04 21:27:28 -0700

    Added getThreatTypes query

--- src/main/java/com/pivotguard/pivotguard_api/repositories/ThreatTypeRepository.java
+++ src/main/java/com/pivotguard/pivotguard_api/repositories/ThreatTypeRepository.java
@@ -4,13 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.pivotguard.pivotguard_api.entities.ThreatType;
+import com.pivotguard.pivotguard_api.dtos.ThreatDto;
+
+import java.util.List;
 
 public interface ThreatTypeRepository extends JpaRepository<ThreatType, Integer> {
 
   @Query (nativeQuery = true, value = """
-      SELECT EXISTS (
-        SELECT 1 FROM Site cs WHERE cs.URL = :currentSite
-      );
-      """)
-    Boolean isCompromised(String currentSite);
+            SELECT tt.Name, tt.Description
+            FROM ThreatType tt
+                JOIN Threat ON (tt.ID = Threat.ThreatID)
+                JOIN Site ON (Threat.SiteID = Site.ID)
+            WHERE Site.URL = :url
+            """)
+    List<ThreatDto> getThreatTypes(String url);
 }
