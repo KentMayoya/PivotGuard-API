cd740fe53bc8b5bee335bbb312bde24a156e35e4
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-04-04 17:07:18 -0700

    Create query to check if URL is compromised

--- src/main/java/com/pivotguard/pivotguard_api/repositories/CompromisedSiteRepository.java
+++ src/main/java/com/pivotguard/pivotguard_api/repositories/CompromisedSiteRepository.java
@@ -1,10 +1,16 @@
 package com.pivotguard.pivotguard_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pivotguard.pivotguard_api.entities.CompromisedSite;
 
 public interface CompromisedSiteRepository extends JpaRepository<CompromisedSite, Integer> {
 
-    
+  @Query (nativeQuery = true, value = """
+            SELECT EXISTS (
+                SELECT 1 FROM Compromised_Site cs WHERE cs.ULR = :currentSite
+            );
+      """)
+    Boolean isCompromised(String currentSite);
 }
